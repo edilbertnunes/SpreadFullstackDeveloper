63777c196d37d092b6fb6fbe540e2aa301ef552a
Edilbert Nunes <12925469+edilbertnunes@users.noreply.github.com>
2022-06-19 12:28:55 -0300

    Add files via upload

---
+++ encurtador_url/README.md
@@ -0,0 +1,9 @@
+# url-shortener Bootcampo Spread/DIO
+
+Projeto criado para construção de um Encurtador de URL.
+
+## Rodando o projeto
+
+- `npm install`
+- `npm run build`
+- `npm run dev`
---
+++ encurtador_url/src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+
+	API_URL: 'http://localhost:3000',
+	MONGO_CONNECTION:
+		'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/banco?retryWrites=true&w=majority',
+}
